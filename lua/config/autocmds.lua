4eb664b8421d64695de81554856b9bda631ac51d
Damixn31 <damianol29@hotmail.com>
2023-11-16 07:28:03 -0300

    updateChange

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- desactivar el modo pegar al salir del insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- fix conceallevel for json files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.conceallevel = 0
+  end,
+})
